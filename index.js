0701e05796c4214ea69b421c112a7535e9722ed5
Bibek <bibek.roman1@gmail.com>
2023-05-15 23:56:16 +0530

    Ecommerce Api

---
+++ index.js
@@ -0,0 +1,21 @@
+const { error } = require("console");
+const express=require("express")
+const app=express()
+const mongoose=require("mongoose")
+const dotenv=require("dotenv")
+const userRoute=require("./routes/user");
+const authRoute=require("./routes/auth")
+const { json } = require("body-parser");
+dotenv.config();
+app.use(express.json())
+mongoose.connect(
+    process.env.MONGO_URL)
+    .then(()=>console.log("DBconnection Successful")).catch((err)=>{
+        console.log(err)});
+
+        app.use("/api/users",userRoute);
+        app.use("/api/auths",authRoute);
+app.listen(process.env.PORT || 5000,()=>{
+    console.log("Server started at port 5000")
+})
+
