2e4938b29e569774047578b95d8630a2d84ac19b
Bibek <bibek.roman1@gmail.com>
2023-05-25 01:05:59 +0530

    Backend completed

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const mongoose=require("mongoose")
 const dotenv=require("dotenv")
 const userRoute=require("./routes/user");
 const authRoute=require("./routes/auth")
+const productRoute=require("./routes/product")
+const orderRoute=require("./routes/order")
+const cartRoute=require("./routes/cart")
+
 const { json } = require("body-parser");
 dotenv.config();
 app.use(express.json())
@@ -15,6 +19,9 @@ mongoose.connect(
 
         app.use("/api/users",userRoute);
         app.use("/api/auths",authRoute);
+        app.use("/api/products",productRoute);
+        app.use("/api/orders",orderRoute);
+        app.use("/api/carts",cartRoute);
 app.listen(process.env.PORT || 5000,()=>{
     console.log("Server started at port 5000")
 })
