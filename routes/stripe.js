07c12ae694f730172a52aa2f9848afa21b234297
Bibek Prasad Yadav <79017118+BibekPrasadYadav@users.noreply.github.com>
2024-01-23 23:43:38 +0530

    Update stripe.js

--- routes/stripe.js
+++ routes/stripe.js
@@ -9,7 +9,7 @@ const stripe = require("stripe")(KEY)
 //         source:req.body.tokenId,
 //         amount:req.body.amount,
 //         currency:"usd",
-//     },(stripeErr,stripeRes)=>{
+//     },(stripeErr,stripeRes)=>{ 
 //         if(stripeErr){
 //             res.status(500).json(stripeErr)
 //         }else{
