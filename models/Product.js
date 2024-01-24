0013ac03d3ca4728ff177e657589fed2612373f7
Bibek Prasad Yadav <79017118+BibekPrasadYadav@users.noreply.github.com>
2024-01-24 23:49:47 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -11,7 +11,7 @@ const ProductSchema=new mongoose.Schema(
         color:{type:Array},
         price:{type:Number,required:true},
         inStock:{type:Boolean,default:true}
-        
+    
     },
     {timestamps:true}
 );
